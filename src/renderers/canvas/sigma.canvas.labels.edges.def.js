1ead444a7cc19fbd9cc73c8ccb68e8206e1f8555
Sergey Sidorchick <whitekrokodile@gmail.com>
2014-05-09 22:09:45 +0300

    Add edge labels rendering for self-referenced nodes.

--- src/renderers/canvas/sigma.canvas.labels.edges.def.js
+++ src/renderers/canvas/sigma.canvas.labels.edges.def.js
@@ -35,7 +35,7 @@
       (edge.color || settings('defaultEdgeColor')) :
       settings('defaultLabelColor');
 
-    if (edge.type === 'curve')
+    if (edge.type === 'curve' || edge.type === 'curvedArrow')
       position = getCurveRenderPosition(source, target, prefix, fontSize);
     else
       position = getStraightRenderPosition(source, target, prefix, fontSize);
@@ -48,29 +48,36 @@
   };
 
   var getCurveRenderPosition = function(source, target, prefix, fontSize) {
-    // Quadratic Bezier curve points.
-    var p = [];
-    p[0] = {
-      x: source[prefix + 'x'],
-      y: source[prefix + 'y']
-    };
-    p[1] = {
-      x: (source[prefix + 'x'] + target[prefix + 'x']) / 2 + (target[prefix + 'y'] - source[prefix + 'y']) / 4,
-      y: (source[prefix + 'y'] + target[prefix + 'y']) / 2 + (source[prefix + 'x'] - target[prefix + 'x']) / 4
-    };
-    p[2] = {
-      x: target[prefix + 'x'],
-      y: target[prefix + 'y']
-    };
+    var sX = source[prefix + 'x'],
+        sY = source[prefix + 'y'],
+        tX = target[prefix + 'x'],
+        tY = target[prefix + 'y'],
+        sSize = source[prefix + 'size'],
+        x,
+        y;
 
-    var t = 0.5; // The relative middle value of a Quadratic Bezier curve.
-    var curveMidX = (1 - t) * (1 - t) * p[0].x + 2 * (1 - t) * t * p[1].x + t * t * p[2].x;
-    var curveMidY = (1 - t) * (1 - t) * p[0].y + 2 * (1 - t) * t * p[1].y + t * t * p[2].y;
+    if (source.id === target.id) {
+      x = sX - 5 * Math.pow(sSize, 0.85) + fontSize;
+      y = sY + 5 * Math.pow(sSize, 0.85) + fontSize;
+    } else {
+      // Quadratic Bezier curve points.
+      var p = [
+            { x: sX, y: sY },
+            {
+              x: (sX + tX) / 2 + (tY - sY) / 4,
+              y: (sY + tY) / 2 + (sX - tX) / 4
+            },
+            { x: tX, y: tY }
+          ],
+          t = 0.5, // The relative middle value of a Quadratic Bezier curve.
+          curveMidX = (1 - t) * (1 - t) * p[0].x + 2 * (1 - t) * t * p[1].x + t * t * p[2].x,
+          curveMidY = (1 - t) * (1 - t) * p[0].y + 2 * (1 - t) * t * p[1].y + t * t * p[2].y,
+          // Calculating offset from the middle of the curve.
+          slopeMidControl = -(p[1].y - curveMidY) / (p[1].x - curveMidX);
 
-    // Calculating offset from the middle of the curve.
-    var slopeMidControl = -(p[1].y - curveMidY) / (p[1].x - curveMidX);
-    var x = curveMidX + Math.abs((5 + fontSize / 4) / Math.sqrt(Math.pow(slopeMidControl, 2) + 1));
-    var y = curveMidY - Math.abs((slopeMidControl * (5 + fontSize / 4)) / Math.sqrt(Math.pow(slopeMidControl, 2) + 1));
+      x = curveMidX + Math.abs((5 + fontSize / 4) / Math.sqrt(Math.pow(slopeMidControl, 2) + 1));
+      y = curveMidY - Math.abs((slopeMidControl * (5 + fontSize / 4)) / Math.sqrt(Math.pow(slopeMidControl, 2) + 1));
+    }
 
     return { x: x, y: y };
   };
